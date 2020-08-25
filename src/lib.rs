fd56e717132c7521d50e57b36394a9f460f9d617
Victor Suriel <victorv.suriel@gmail.com>
2020-08-25 17:31:20 -0400

    added render function

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,69 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
+static MAX_HEIGHT: u32 = 800;
+static MAX_WIDTH: u32 = 1280;
+
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, mbrot!");
+#[derive(Clone, Copy, Debug)]
+pub struct Complex {
+    re: f32,
+    img: f32,
+}
+
+
+#[wasm_bindgen]
+impl Complex {
+    pub fn new(re: f32, img: f32) -> Complex {
+        Complex{re, img}
+    }
+}
+
+
+#[wasm_bindgen]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[repr(C)]
+pub struct RGBA {
+    r: u8,
+    g: u8,
+    b: u8,
+    a: u8,
+}
+
+
+#[wasm_bindgen]
+pub struct Universe {
+    width: u32,
+    height: u32,
+    pixels: Vec<RGBA>,
+}
+
+
+#[wasm_bindgen]
+impl Universe {
+    pub fn new(width: u32, height: u32) -> Universe {
+        let width = if width > MAX_WIDTH {MAX_WIDTH} else {width};
+        let height = if height > MAX_HEIGHT {MAX_HEIGHT} else {height};
+        let max_pixels = MAX_WIDTH * MAX_HEIGHT;
+
+        let mut pixels: Vec<RGBA> = Vec::with_capacity(max_pixels as usize);
+        for _ in 0..width*height {
+            pixels.push(RGBA{r:0, g:255, b:0, a:255});
+        }
+        Universe{width, height, pixels}
+    }
+
+
+    pub fn render(&mut self, anchor: Complex, dx: f32) {
+        for (i, e) in self.pixels.iter_mut().enumerate() {
+            e.g = 0;
+            e.r = 255;
+        }
+    }
+
+
+    pub fn pixels(&self) -> *const RGBA {
+        self.pixels.as_ptr()
+    }
 }
