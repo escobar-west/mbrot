a6dc93b74b74cd3f6cf3dcd0b0429d6a83cfdc93
Victor Suriel <victorv.suriel@gmail.com>
2020-08-26 16:58:08 -0400

    removed more mult ops

--- src/lib.rs
+++ src/lib.rs
@@ -62,42 +62,40 @@ impl Universe {
         let mut y: f32;
         let mut x2: f32;
         let mut y2: f32;
-        let mut counter: u32;
+        let mut iter_counter: u32;
 
         let x_off = center.re - dx * self.width as f32 * 0.5;
         let y_off = center.img - dx * self.height as f32 * 0.5;
 
-        let mut px_idx: usize = 0;
+        let mut pix_idx: usize = 0;
         let mut pix_coord = Complex{re: x_off,
                                     img: y_off};
 
         for _row in 0..self.height {
-            pix_coord.re = x_off;
-
             for _col in 0..self.width { 
                 x = 0.0;
                 y = 0.0;
                 x2 = 0.0;
                 y2 = 0.0;
-                counter = 0;
+                iter_counter = 0;
 
-                while (x2 + y2 < 4.0) && (counter < max_iter) {
+                while (x2 + y2 < 4.0) && (iter_counter < max_iter) {
                     y = (x + x) * y + pix_coord.img;
                     x = x2 - y2 + pix_coord.re;
                     x2 = x * x;
                     y2 = y * y;
-                    counter += 1;
+                    iter_counter += 1;
                 }
-
                 unsafe {
-                    let pix = self.pixels.get_unchecked_mut(px_idx);
-                    pix.r = if counter < max_iter {155} else {0};
-                    pix.b = if counter < max_iter {255} else {0};
-                    pix.g = if counter < max_iter {255} else {0};
+                    let pix = self.pixels.get_unchecked_mut(pix_idx);
+                    pix.r = if iter_counter < max_iter {155} else {0};
+                    pix.b = if iter_counter < max_iter {255} else {0};
+                    pix.g = if iter_counter < max_iter {255} else {0};
                 }
                 pix_coord.re += dx;
-                px_idx += 1;
+                pix_idx += 1;
             }
+            pix_coord.re = x_off;
             pix_coord.img += dx;
         }
     }
