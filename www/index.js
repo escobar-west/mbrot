c546771aa2857d1c0bc80133ce38ce1f3f6f0fb6
Victor Suriel <victorv.suriel@gmail.com>
2020-08-27 11:51:42 -0400

    added red box thing

--- www/index.js
+++ www/index.js
@@ -3,9 +3,31 @@ import {memory} from "mbrot/mbrot_bg";
 
 const canvas = document.getElementById('mbrot-canvas');
 const ctx = canvas.getContext('2d');
-
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+var box = null;
+
+canvas.onmousedown = function(e) {
+    if (box == null) {
+        box = [e.clientX, e.clientY, 0, 0];
+    }
+}
+
+canvas.onmousemove = function(e) {
+    if (box != null) {
+        ctx.lineWidth = 1;
+        //ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.strokeStyle= '#FF3B03';
+        box[2] = e.clientX;
+        box[3] = e.clientY;
+        ctx.strokeRect(box[0], box[1], box[2]-box[0], box[3]-box[1]);
+    }
+}
+
+canvas.onmouseup = function(e) {
+    box = null;
+}
 
 const universe = Universe.new(canvas.width, canvas.height);
 const cval = Complex.new(0.377, 0.2);
@@ -29,4 +51,4 @@ const draw = () => {
 }
 
 draw();
-renderLoop();
+//renderLoop();
